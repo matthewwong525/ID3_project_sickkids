71d882aee647f29a1435b47f8ebbc5ffdd4e7e9c
Matt Wong <matt@godfreds-air.sickkids.ca>
2019-02-11 12:59:23 -0500

    Add class implementation for ID3 and documentation of code

---
+++ ID3_Class.py
@@ -0,0 +1,188 @@
+from __future__ import division
+import math
+from anytree import Node, RenderTree, LevelOrderIter
+from anytree.exporter import DotExporter
+from ga4gh_API import API
+
+class ID3:
+
+    def __init__(self):
+        """
+        Initializes the ID3 class
+
+        Attributes:
+        api (API): API object that is used to interact with the virtual API
+        root_node (Node): Creates the root node of the tree to be added upon
+        """
+        self.api = API()
+        subset = self.api.get_target_set()
+        self.root_node = self.ID3(Node('root'), subset)
+        self.clean_tree()
+
+    @staticmethod
+    def get_subset_count(subset):
+        """
+        Args:
+            subset (dict): A dictionary containing keys of ancestries and values of the counts for the particular ancestry
+
+        Returns:
+            ret_str: A string containing counts of the subset and the name of the most common subset
+        """
+        max_key = str(max(subset, key=subset.get))
+        total_count = str(sum(subset.values()))
+        ret_str = "most common ancestry: %s,%s | total count: %s" % (max_key, subset[max_key], total_count)
+        return ret_str
+
+    @staticmethod
+    def entropy_by_count(subset):
+        """
+        Gets the entropy given an input of a subset
+
+        Args:
+            subset (dict): A dictionary containing keys of ancestries and values of the counts for the particular ancestry
+
+        Returns:
+            entropy (float): the entropy value of the subset used for the ID3 algorithm
+        """
+        total_count = sum(subset.values())
+        probability_list = []
+        entropy = 0
+        if total_count == 0:
+            return 0
+        for value in subset.values():
+            probability = value / total_count
+            if value != 0 : entropy -= (math.log(probability, 2) * probability)
+        return entropy
+
+    @staticmethod
+    def create_split_path(split_path, new_variant_name):
+        """
+        Creates two new split paths given the variant name and the split path
+
+        Args:
+            subset (dict): A dictionary containing keys of ancestries and values of the counts for the particular ancestry
+
+        Returns:
+            w_split_path: The split path with the variant
+            wo_split_path: The split path without the variant
+
+        """
+        w_split_path = (list(split_path[0]), list(split_path[1]))
+        wo_split_path = (list(split_path[0]), list(split_path[1]))
+        w_split_path[0].append(new_variant_name)
+        wo_split_path[0].append(new_variant_name)
+        w_split_path[1].append(1)
+        wo_split_path[1].append(0)
+
+        return w_split_path, wo_split_path
+
+
+    def is_leaf_node(self, subset, split_path, split_index):
+        """
+        Checks if the node is a leaf node given a subset
+
+        Args:
+            subset (dict): A dictionary containing keys of ancestries and values of the counts for the particular ancestry
+            split_paths (list1, list2): 
+                This is the paths of the splits before the current split. The first list
+                is the list of variant names and the second list is the direction
+                of the split. The direction of the second list is depicted by 1's
+                and 0's. Where 1 is splitting in the direction with the variant
+                and 0 is splitting in the direction without the variant. 
+            split_index (int): the index for the next split to be split on
+        Returns:
+            Boolean Value: the boolean value represents whether or not the node is a leaf node
+
+        """
+        # check if all variants are of one ancestry (Essentially if all remaining variants(attributes) contains one region(value))
+        attr_list = set()
+        upd_var_count = { k : v for k, v in subset.items() if v != 0 }
+        attr_list.update(upd_var_count.keys())
+        if len(attr_list) == 1 or len(split_path[0]) >= len(self.api.variant_name_list) or split_index is None:
+        #if len(attr_list) == 1 or len(split_path[0]) >= 1 or split_index is None or ID3.entropy_by_count(subset) == 0:
+            return True
+        return False
+
+    def print_tree(self, file_name):
+        DotExporter(self.root_node).to_picture("%s.png" % file_name)
+
+    def find_variant_split(self, subset, split_path):
+        """
+        Finds the variant to split on and returns the index where it should be split on.
+        This calculation is based on which attribute gives the greatest information gain.
+
+        Args:
+            subset (dict): A dictionary containing keys of ancestries and values of the counts for the particular ancestry
+            split_paths (list1, list2): 
+                This is the paths of the splits before the current split. The first list
+                is the list of variant names and the second list is the direction
+                of the split. The direction of the second list is depicted by 1's
+                and 0's. Where 1 is splitting in the direction with the variant
+                and 0 is splitting in the direction without the variant. 
+
+        Returns:
+            ret_index: index that yields the greatest information gain
+        """
+        variant_list = self.api.split_subset(split_path)[2]
+        total_count = sum(subset.values())
+        ret_index = 0
+        final_info_gain = 0
+        var_idx_list = [self.api.variant_name_list.index(var_name) for var_name in split_path[0]]
+
+        if total_count == 0:
+            return None
+
+        # loops through all the counts for each variant
+        for idx, w_var_counts in enumerate(variant_list):
+            wo_var_counts = { k : subset.get(k, 0) - w_var_counts.get(k, 0) for k in subset.keys() + w_var_counts.keys() }
+            info_gain = ID3.entropy_by_count(subset)
+            # calculates info gain
+            for popu in w_var_counts.keys():
+                info_gain -= wo_var_counts[popu] / total_count * ID3.entropy_by_count(w_var_counts)
+            # finds max info gain and checks if the index is not in excluded variants list
+            if final_info_gain < info_gain and idx not in var_idx_list:
+                final_info_gain = info_gain
+                ret_index = idx
+            #print str(info_gain) + " : " + self.api.variant_name_list[idx]
+        return ret_index
+
+    # note: variant list must be same length as count list
+    def ID3(self, node, subset, split_path=([], [])):
+        """
+        A recursive function that creates a tree given the root node and a subset
+
+        Note: variant list must be same length as count list
+
+        Args:
+            node (Node): A node object from the anytree library
+            subset (dict): A dictionary containing keys of ancestries and values of the counts for the particular ancestry
+            split_paths (list1, list2): 
+                This is the paths of the splits before the current split. The first list
+                is the list of variant names and the second list is the direction
+                of the split. The direction of the second list is depicted by 1's
+                and 0's. Where 1 is splitting in the direction with the variant
+                and 0 is splitting in the direction without the variant. 
+
+        TODO:
+            * Delete the extra nodes in the tree where the tree doesn't split
+
+        """
+        # find the attrivute to split on and adds that variant to exclude variant list
+        split_index = self.find_variant_split(subset, split_path)
+        if not self.is_leaf_node(subset, split_path, split_index):
+            var_name = self.api.variant_name_list[split_index]
+            
+            w_subset, wo_subset = self.api.split_subset(split_path, var_name)[:2]
+
+            w_split_path, wo_split_path = ID3.create_split_path(split_path, var_name)
+
+
+            if sum(w_subset.values()) > 0:
+                self.ID3(Node("with:%s\n%s" % (var_name, ID3.get_subset_count(w_subset)), parent=node), dict(w_subset), w_split_path)
+            if sum(wo_subset.values()) > 0:
+                self.ID3(Node("without:%s\n%s" % (var_name, ID3.get_subset_count(wo_subset)), parent=node), dict(wo_subset), wo_split_path)
+            return node
+
+if __name__ == "__main__":
+    id3_alg = ID3()
+    id3_alg.print_tree('udo1')
---
+++ ga4gh_API.py
@@ -0,0 +1,171 @@
+import vcf
+
+class API:
+    def __init__(self):
+        """
+        Initializes the API class
+
+        NOTE:
+            api_variant_list, api_indiv_list, and api_popu_list should be of the same
+            length and every element in this list corresponds to a particular person
+
+        Attributes:
+            api_variant_list (list): Represents the variants in each person. 
+                                     The value of the variants is a list of 0's and 
+                                     1's indicating if the variant exists in the person.
+            api_indiv_list (list): Represents the individual code of a person
+            api_popu_list (list): Represents the ancestry of the person
+            api_variant_name_list (list): All the variant names queried
+            api_variant_name_list (list): Names of the variants in the format of
+                                          "VARIANT_POS,VARIANT_REF,VARIANT_ALT"
+            ancestry_dict (dict): a dictionary which maps indivdual ID to population
+
+        TODO:
+            * Add option to query for different variants rather than grabbing a predefined set of variants
+        """
+        self.variant_list = []
+        self.indiv_list = []
+        self.popu_list = []
+        self.variant_name_list = []
+        self.ancestry_dict = {}
+
+        # fetch variants from vcf and create a dictionary
+        vcf_reader = vcf.Reader(open('chr22.vcf', 'r'))
+        variants = vcf_reader.fetch('22', 16050074, 16050075+150000)
+        variant_dict = self.create_variant_dict(variants)
+
+        # updates variables
+        self.read_user_mappings(variant_dict)
+
+    def create_variant_dict(self, variants):
+        """
+        Creates a ditionary of variants from a vcf file which is used to update variables
+        within this class
+
+        Args:
+            variants (Reader): A Reader Object from the PyVCF library which contains variants
+
+        Returns:
+            variant_dict (dict): A dictionary where the
+                                    key: is the individual ID
+                                    value: is a list of binary values where
+                                           (1 = variant exists, 0 = variant doesn't exist)
+        """
+        variant_dict = {}
+        variant_name_list = []
+        idx = 0
+        # loops through variants
+        for variant in variants:
+            idx += 1
+            self.variant_name_list.append(','.join([str(variant.POS), str(variant.REF), str(variant.ALT)]))
+            # loops through people in variants
+            for call in variant.samples:
+                variant_dict[call.sample] = variant_dict.get(call.sample, [])
+                # checks if variant exists in person
+                if call['GT'] == '0|0':
+                    variant_dict[call.sample].append(0)
+                else:
+                    variant_dict[call.sample].append(1)
+        return variant_dict
+
+    def read_user_mappings(self, variant_dict):
+        """
+        Reads the usermappings from a file and updates the variables in the class
+        """
+        with open('user_map.ped') as file:
+            next(file)
+            for line in file:
+                split_line = line.split('\t')
+                indiv_id = split_line[1]
+                population = split_line[6]
+
+                self.ancestry_dict[indiv_id] = population
+
+                # checks if variant individual is in the variant dict from the vcf file
+                if indiv_id in variant_dict:
+                    self.indiv_list.append(indiv_id)
+                    self.popu_list.append(population)
+                    self.variant_list.append(variant_dict[indiv_id])
+
+
+    # splits the set given a variant 
+    # returns 2 subsets of the data
+    def split_subset(self, split_paths=([], []), split_var=None):
+        """
+        Splits the subset given the path of the splits before and a
+        variable to split on.
+
+        Attributes:
+            split_paths (list1, list2): 
+                This is the paths of the splits before the current split. The first list
+                is the list of variant names and the second list is the direction
+                of the split. The direction of the second list is depicted by 1's
+                and 0's. Where 1 is splitting in the direction with the variant
+                and 0 is splitting in the direction without the variant.
+            split_var (string): The variant name it is now splitting on
+
+        Returns:
+            w_variant_dict (dict): The split subset that includes the variant
+            wo_variant_dict (dict): The split subset that does not include the variant
+            w_variant_list: 
+                A list representing of a dictionary of ancestry counts per variant
+                [
+                    {'GBR': 5, 'CML': 0, 'ABC': 3 ...}
+                    ...
+                    ..
+                    .
+                ]
+
+        """
+        # retrieves variant from "API"
+        ancestry_list = set(self.ancestry_dict.values())
+        w_variant_list = [dict.fromkeys(ancestry_list, 0) for variant_names in self.variant_name_list]
+        wo_variant_list = [dict.fromkeys(ancestry_list, 0) for variant_names in self.variant_name_list]
+        w_variant_dict = dict.fromkeys(ancestry_list, 0)
+        wo_variant_dict = dict.fromkeys(ancestry_list, 0)
+
+        ignore_rows_idxs = []
+
+        # basically find rows to ignore because it has been split upon already
+        for exc_var, direction in zip(split_paths[0], split_paths[1]):
+            # finding rows to ignore
+            var_idx = self.variant_name_list.index(exc_var)
+            # looping through all the people
+            for idx, variants in enumerate(self.variant_list):
+                if idx not in ignore_rows_idxs and variants[var_idx] is not direction:
+                    ignore_rows_idxs.append(idx)
+
+        # create new subset after finding all the rows to ignore
+        for idx, variants in enumerate(self.variant_list):
+            if idx not in ignore_rows_idxs:
+                popu = self.popu_list[idx]
+                # check if split_var is null
+                if split_var:
+                    f_var_idx = self.variant_name_list.index(split_var)
+                    if variants[f_var_idx] is 1:
+                        w_variant_dict[popu] += 1
+                    else:
+                        wo_variant_dict[popu] += 1
+                # find counts of variants
+                for idx2, variant in enumerate(variants):
+                    if variant is 1: 
+                        w_variant_list[idx2][popu] += 1
+
+
+        return w_variant_dict, wo_variant_dict, w_variant_list
+
+    def get_target_set(self):
+        """
+        Gets the target subset
+
+        Returns:
+            counts (dict): A dictionary containing keys of ancestries and values of the counts for the particular ancestry
+        """
+        ancestry_list = set(self.ancestry_dict.values())
+        counts = dict.fromkeys(ancestry_list, 0)
+        for i in self.popu_list:
+            counts[i] = counts.get(i, 0) + 1
+        return counts
+
+
+
