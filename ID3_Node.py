eb02de65de38f7196ef85d0163aacf92a4b9c719
Matt Wong <matt@godfreds-air.sickkids.ca>
2019-02-20 09:27:40 -0500

    Add ID3 Node class

---
+++ ID3_Node.py
@@ -0,0 +1,21 @@
+from anytree import NodeMixin
+
+class ID3_Node(NodeMixin):
+    def __init__(self, variant_name, subset, with_variant, parent=None, children=None):
+        super(ID3_Node, self).__init__()
+        self.variant_name = variant_name
+        self.with_variant = with_variant
+        self.subset = subset
+        self.total_count = str(sum(subset.values()))
+        self.most_common_ancestry = str(max(subset, key=subset.get))
+        self.parent = parent
+        if children:
+            self.children = children
+
+    @staticmethod
+    def name_func(node):
+        return "%s: %s \n %s" % ( 'with' if node.with_variant else 'w/o', node.variant_name, ID3_Node.nodeattrfunc(node))
+
+    @staticmethod
+    def nodeattrfunc(node):
+        return "most common ancestry: %s | total count: %s" % (node.most_common_ancestry, node.total_count)
