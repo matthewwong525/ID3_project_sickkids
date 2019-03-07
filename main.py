26a91fd7e5ed927494f945e38474e032bd289a65
Matt Wong <matt@godfreds-air.sickkids.ca>
2019-03-07 09:33:14 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from ID3_Class import ID3
+
+# Creates ID3 object with a filepath to the config.json
+id3_obj = ID3('config.json', local=True)
+
+# prints the ID3 tree as `tree.png`
+id3_obj.print_tree('tree')
+
+# prints the list of all the variant names
+print(id3_obj.api.variant_name_list)
+
+# predicts ancestry of the person with the variant `22:50121766:50121767` and no other variant in `variant_name_list`
+id3_obj.predict(['22:50121766:50121767'])
